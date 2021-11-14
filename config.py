b799e3d4950013938b89e512301142e0eff82a51
CameronBBailey <CameronBradBailey@gmail.com>
2021-11-14 14:45:24 -0500

    Day 2 checkpoint

--- config.py
+++ config.py
@@ -4,5 +4,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config():
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'The secret key'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEPLOY_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACH_MODIFICATIONS = False
+    FLASK_APP = os.environ.get('FLASK_APP')
+    FLASK_ENV = os.environ.get('FLASK_ENV')
+    
